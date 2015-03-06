28e8bb1b3cfcbc263768ac6692434fe9976d1fac
co <corentin.regal@gmail.com>
2015-03-06 11:50:14 +0100

    hexaground

--- script/control.py
+++ script/control.py
@@ -7,6 +7,8 @@ from librt import *
 def control(path_conf):
     with open(path_conf, 'r') as f:
         conf = json.loads(f.read())
+        # o = conf['scene']['lights'][0]['bulb']
+        o = conf['eye']
 
     DEP = 5
     ROT = 0.1
@@ -25,36 +27,41 @@ def control(path_conf):
             break
         # Move
         elif cmd == 'w':
-            conf['eye']['pos']['z'] -= DEP
+            o['pos']['z'] -= DEP
         elif cmd == 's':
-            conf['eye']['pos']['z'] += DEP
+            o['pos']['z'] += DEP
         elif cmd == 'a':
-            conf['eye']['pos']['x'] -= DEP
+            o['pos']['x'] -= DEP
         elif cmd == 'd':
-            conf['eye']['pos']['x'] += DEP
+            o['pos']['x'] += DEP
         elif cmd == 'q':
-            conf['eye']['pos']['y'] += DEP
+            o['pos']['y'] += DEP
         elif cmd == 'e':
-            conf['eye']['pos']['y'] -= DEP
+            o['pos']['y'] -= DEP
         # Rotate
         elif cmd == 'o':
-            conf['eye']['dir']['x'] -= ROT
+            o['dir']['x'] -= ROT
         elif cmd == 'l':
-            conf['eye']['dir']['x'] += ROT
+            o['dir']['x'] += ROT
         elif cmd == 'k':
-            conf['eye']['dir']['y'] += ROT
+            o['dir']['y'] += ROT
         elif cmd == ';':
-            conf['eye']['dir']['y'] -= ROT
+            o['dir']['y'] -= ROT
         elif cmd == 'i':
-            conf['eye']['dir']['z'] += ROT
+            o['dir']['z'] += ROT
         elif cmd == 'p':
-            conf['eye']['dir']['z'] -= ROT
+            o['dir']['z'] -= ROT
+        # Save
+        elif cmd == 'n':
+            path = input('path> ')
+            with open(path, 'w+') as f:
+                f.write(json.dumps(conf, indent=4))
         # Ignored
         else:
             print('Command ignored')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Usage: ./control.py [config.json]')
+        print('Usage: ./control.py [scene.json]')
         sys.exit(1)
     control(sys.argv[1])
---
+++ script/hexaground.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import sys
+import json
+from math import tan, pi
+from random import uniform
+
+def get_config():
+    return {
+        "picture": {
+            "w": 200,
+            "h": 200,
+            "path": "image/hexaground.png"
+        },
+        "eye": {
+            "pos": { "x": -5, "y": 20, "z": 35 },
+            "dir": { "x": -0.7, "y": -0.1, "z": 0 },
+            "fov": 2.1
+        },
+        "scene": {
+            "objects": [
+            ],
+            "lights": [
+                {
+                    "bulb": {
+                        "diff": 0.9,
+                        "spec": 1.5,
+                        "shin": 20,
+                        "pos": {
+                            "x": 25,
+                            "y": 50,
+                            "z": 5
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+def get_color():
+    return { "r": uniform(0, 1), "g": uniform(0, 1), "b": uniform(0, 1) }
+
+def get_height(min, max):
+    return uniform(min, max)
+
+def hexaground():
+    config = get_config()
+
+    COUNT_X=8
+    COUNT_Z=10
+    SZ=10
+    MIN_H=1
+    MAX_H=8
+
+    for z in range(-COUNT_Z // 2, COUNT_Z // 2):
+        for x in range(-COUNT_X // 2, COUNT_X // 2):
+            hexa = {
+                "aahexa": {
+                    "pos": { "x": x * SZ + (z % 2) * SZ / 2., "y": 0, "z": z * tan(pi / 3) * SZ / 2. },
+                    "x": SZ,
+                    "y": get_height(MIN_H, MAX_H),
+                    "mat": {
+                        "color": get_color(),
+                        "spec": 0.4,
+                        "diff": 1
+                    }
+                }
+            }
+            config['scene']['objects'].append(hexa)
+
+    print(json.dumps(config, indent=4))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        print('Usage: ./hexaground.py')
+        sys.exit(1)
+    hexaground()
